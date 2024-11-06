32fc01df9204c3f0f36fe98f1ae8a8203d6da118
Kartik Dhiman <133855202+KDhiman123@users.noreply.github.com>
2024-11-06 09:56:53 +0530

    Add files via upload

---
+++ module2.mjs
@@ -0,0 +1,10 @@
+const a="Kartik";
+const b="Rohan";
+const c="Aakash";
+const d="Priyanka";
+
+export default a;
+// for non-default attributes, we use curly braces.CONCEPT OF JS
+export {b};
+export {c};
+export {d};
